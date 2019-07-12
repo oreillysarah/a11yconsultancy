27a3435ba66362be1c3438ec77ec3d850556edf0
oreillysarah <35141745+oreillysarah@users.noreply.github.com>
2019-07-12 14:15:12 +0100

    Add files via upload

--- site/form-to-email.php
+++ site/form-to-email.php
@@ -10,7 +10,7 @@ $subject =$_POST['subject'];
 //Validate first
 if(empty($name||empty($email)||empty($subject))
 {
-    echo "name, emai and subject are required";
+    echo "name, email and subject are required";
     exit;
 }
 
@@ -25,7 +25,35 @@ $headers ="From: $email_from \r\n";
 mail($to,$subject,$email_body,$headers);
 //done direct to thank you page
 
+//protect against spammers
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
 
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{   echo "Bad email value!";
+    exit;
+}
 
 ?>
 
